95a11a9e0e45e9eceeadf30ec0509d1026674c34
Dave Goddard <dave@goddard.id.au>
2016-10-19 13:29:02 -0400

    Do some timestamp updates
    Broken: apps_hashcode (and probably others)

--- src/eureka.cr
+++ src/eureka.cr
@@ -12,6 +12,14 @@ all_apps = Hash(String, Hash(String, JSON::Type)).new
 post "/eureka/apps/:appID" do |env|
   instance = env.params.json["instance"]
   if instance.is_a?(Hash)
+    lease_info = instance.fetch("leaseInfo") if instance.is_a?(Hash)
+    if lease_info.is_a?(Hash)
+      lease_info["registrationTimestamp"] = Time.now.epoch_ms
+      lease_info["lastRenewalTimestamp"] = Time.now.epoch_ms + 30_000
+      lease_info["evictionTimestamp"] = Time.now.epoch_ms + 90_000
+      lease_info["serviceUpTimestamp"] = Time.now.epoch_ms
+    end
+
     instanceId = instance.fetch("instanceId").to_s
 
     appID = env.params.url["appID"]
@@ -48,18 +56,35 @@ put "/eureka/apps/:appID/:instanceID" do |env|
   ## Necessary
   ## https://github.com/Netflix/eureka/blob/4f24d9dcf90713b421f6c092fa63e707cbf74df3/eureka-core/src/main/java/com/netflix/eureka/registry/AbstractInstanceRegistry.java#L345
 
+  instance["status"] = env.params.query["status"].to_s if instance.is_a?(Hash)
+
+  lease_info = instance.fetch("leaseInfo") if instance.is_a?(Hash)
+  if lease_info.is_a?(Hash)
+    lease_info["lastRenewalTimestamp"] = Time.now.epoch_ms + 30_000
+    lease_info["evictionTimestamp"] = Time.now.epoch_ms + 90_000
+    lease_info["serviceUpTimestamp"] = Time.now.epoch_ms
+  end
+
   env.response.status_code = 200
 end
 
 get "/eureka/apps" do |env|
   env.response.status_code = 200
 
-  applications = Array(Hash(String, JSON::Type)).new
+  applications = Array(Hash(String, JSON::Type)).new(0)
+  statuses = Hash(String, UInt32).new(0_u32)
   all_apps.each do |name, instances|
     application = { "name" => name, "instance" => instances.values }
     applications << application
+    instances.each do |instance|
+      status = instance.fetch("status") if instance.is_a?(Hash)
+      statuses[status] += 1 if status.is_a?(String)
+    end
   end
-  { "applications" => { "versions__delta" => 1, "apps_hashcode" => "DOWN_1_UP_2_", "application" => applications }}.to_xml
+
+  apps_hashcode = statuses.keys.sort.map { |k| "#{k}_#{statuses[k]}" }.join("_") + "_"
+
+  { "applications" => { "versions__delta" => 1, "apps_hashcode" => apps_hashcode, "application" => applications }}.to_xml
 end
 
 get "/eureka/apps/:appID" do |env|
